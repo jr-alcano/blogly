a6c678dae90a652e3a0fd01084d421bf6dd1e41d
jr-alcano <134070220+jr-alcano@users.noreply.github.com>
2024-08-09 19:33:16 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-from models import db, connect_db, User, Post
+from models import db, connect_db, User, Post, Tag, PostTag
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://johnalcano:jiBAi677@localhost/blogly'
@@ -12,76 +12,53 @@ with app.app_context():
     db.create_all()
 
 
-@app.route('/')
-def root():
-    return redirect(url_for('list_users'))
+# Existing user and post routes here...
+
+# Add tag routes
+@app.route('/tags')
+def list_tags():
+    tags = Tag.query.all()
+    return render_template('tags.html', tags=tags)
 
 
-@app.route('/users')
-def list_users():
-    users = User.query.all()
-    return render_template('users.html', users=users)
-
-
-@app.route('/users/new', methods=["GET", "POST"])
-def add_user():
+@app.route('/tags/new', methods=["GET", "POST"])
+def add_tag():
     if request.method == "POST":
-        new_user = User(
-            first_name=request.form['first_name'],
-            last_name=request.form['last_name'],
-            image_url=request.form['image_url'] or None)
-        db.session.add(new_user)
+        new_tag = Tag(name=request.form['name'])
+        db.session.add(new_tag)
         db.session.commit()
-        return redirect(url_for('list_users'))
+        return redirect(url_for('list_tags'))
 
-    return render_template('new_user.html')
-
-
-@app.route('/users/<int:user_id>')
-def show_user(user_id):
-    user = User.query.get_or_404(user_id)
-    return render_template('user_detail.html', user=user)
+    return render_template('new_tag.html')
 
 
-@app.route('/users/<int:user_id>/edit', methods=["GET", "POST"])
-def edit_user(user_id):
-    user = User.query.get_or_404(user_id)
+@app.route('/tags/<int:tag_id>')
+def show_tag(tag_id):
+    tag = Tag.query.get_or_404(tag_id)
+    return render_template('tag_detail.html', tag=tag)
+
+
+@app.route('/tags/<int:tag_id>/edit', methods=["GET", "POST"])
+def edit_tag(tag_id):
+    tag = Tag.query.get_or_404(tag_id)
 
     if request.method == "POST":
-        user.first_name = request.form['first_name']
-        user.last_name = request.form['last_name']
-        user.image_url = request.form['image_url'] or None
+        tag.name = request.form['name']
         db.session.commit()
-        return redirect(url_for('list_users'))
+        return redirect(url_for('list_tags'))
 
-    return render_template('edit_user.html', user=user)
+    return render_template('edit_tag.html', tag=tag)
 
 
-@app.route('/users/<int:user_id>/delete', methods=["POST"])
-def delete_user(user_id):
-    user = User.query.get_or_404(user_id)
-    db.session.delete(user)
+@app.route('/tags/<int:tag_id>/delete', methods=["POST"])
+def delete_tag(tag_id):
+    tag = Tag.query.get_or_404(tag_id)
+    db.session.delete(tag)
     db.session.commit()
-    return redirect(url_for('list_users'))
+    return redirect(url_for('list_tags'))
 
 
-# Add post routes
-
-@app.route('/users/<int:user_id>/posts/new', methods=["GET", "POST"])
-def add_post(user_id):
-    user = User.query.get_or_404(user_id)
-
-    if request.method == "POST":
-        new_post = Post(
-            title=request.form['title'],
-            content=request.form['content'],
-            user_id=user.id)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect(url_for('show_user', user_id=user.id))
-
-    return render_template('new_post.html', user=user)
-
+# Update post routes to handle tags
 
 @app.route('/posts/<int:post_id>')
 def show_post(post_id):
@@ -89,17 +66,44 @@ def show_post(post_id):
     return render_template('post_detail.html', post=post)
 
 
+@app.route('/users/<int:user_id>/posts/new', methods=["GET", "POST"])
+def add_post(user_id):
+    user = User.query.get_or_404(user_id)
+    tags = Tag.query.all()
+
+    if request.method == "POST":
+        tag_ids = request.form.getlist('tags')
+        new_post = Post(
+            title=request.form['title'],
+            content=request.form['content'],
+            user_id=user.id)
+        db.session.add(new_post)
+        db.session.commit()
+
+        for tag_id in tag_ids:
+            tag = Tag.query.get(tag_id)
+            new_post.tags.append(tag)
+        db.session.commit()
+
+        return redirect(url_for('show_user', user_id=user.id))
+
+    return render_template('new_post.html', user=user, tags=tags)
+
+
 @app.route('/posts/<int:post_id>/edit', methods=["GET", "POST"])
 def edit_post(post_id):
     post = Post.query.get_or_404(post_id)
+    tags = Tag.query.all()
 
     if request.method == "POST":
         post.title = request.form['title']
         post.content = request.form['content']
+        tag_ids = request.form.getlist('tags')
+        post.tags = [Tag.query.get(tag_id) for tag_id in tag_ids]
         db.session.commit()
         return redirect(url_for('show_post', post_id=post.id))
 
-    return render_template('edit_post.html', post=post)
+    return render_template('edit_post.html', post=post, tags=tags)
 
 
 @app.route('/posts/<int:post_id>/delete', methods=["POST"])
